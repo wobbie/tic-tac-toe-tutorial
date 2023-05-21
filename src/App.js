f29a7fad0d4909dd3e67ee7a0f50b0a62227b6e6
Rob Parker <wobbieparkre@gmail.com>
2023-05-21 23:07:26 +0100

    Fix issue with refactor

--- src/App.js
+++ src/App.js
@@ -48,9 +48,9 @@ function Board({ xIsNext, squares, onPlay }) {
 
 export default function Game() {
 
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
